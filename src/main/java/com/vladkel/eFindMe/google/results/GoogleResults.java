676e9eb24feb246b5996754fcd2aed4d10f99459
Eliott <eliott@mini-eliott.local>
2014-11-27 17:10:12 +0100

    add google basic search tools

---
+++ src/main/java/com/vladkel/eFindMe/google/results/GoogleResults.java
@@ -0,0 +1,9 @@
+package com.vladkel.eFindMe.google.results;
+
+public class GoogleResults {
+	private ResponseData responseData;
+    public ResponseData getResponseData() { return responseData; }
+    public void setResponseData(ResponseData responseData) { this.responseData = responseData; }
+    public String toString() { return "ResponseData[" + responseData + "]"; }
+
+}
---
+++ src/main/java/com/vladkel/eFindMe/google/results/ResponseData.java
@@ -0,0 +1,10 @@
+package com.vladkel.eFindMe.google.results;
+
+import java.util.List;
+
+public class ResponseData {
+    private List<Result> results;
+    public List<Result> getResults() { return results; }
+    public void setResults(List<Result> results) { this.results = results; }
+    public String toString() { return "Results[" + results + "]"; }
+}
