349432c0b451a103bede4362c88aeaeafe7b00fc
Eliott <eliott@mini-eliott.local>
2014-11-28 10:37:35 +0100

    20 research with Google

--- src/test/java/com/vladkel/eFindMe/google/results/SimpleGoogleSearchIT.java
+++ src/test/java/com/vladkel/eFindMe/google/results/SimpleGoogleSearchIT.java
@@ -14,23 +14,25 @@ public class SimpleGoogleSearchIT {
 	
 	public static void webAPI(){
 		try{
-			String address = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&q=";
-			String query = "Caroline Dossal";
-			String charset = "UTF-8";
-	 
-			//URL url = new URL("https://www.google.fr/webhp?&ion=1&espv=2&ie=UTF-8#safe=off&q=eliott%20laversin");
-			URL url = new URL(address + URLEncoder.encode(query, charset));
-			System.out.println(url.toString());
-			Reader reader = new InputStreamReader(url.openStream(), charset);
-			GoogleResults results = new Gson().fromJson(reader, GoogleResults.class);
-	 
-			int total = results.getResponseData().getResults().size();
-			System.out.println("total: " + total);
-	 
-			// Show title and URL of each results
-			for(int i = 0; i <= total-1; i++){
-				System.out.println("Title: " + results.getResponseData().getResults().get(i).getTitle());
-				System.out.println("URL: " + results.getResponseData().getResults().get(i).getUrl() + "\n");
+			for (int r = 0; r < 20; r = r + 4) {
+				String address = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&start=" + r + "&q=";
+				String query = "ELiott Laversin";
+				String charset = "UTF-8";
+		 
+				//URL url = new URL("https://www.google.fr/webhp?&ion=1&espv=2&ie=UTF-8#safe=off&q=eliott%20laversin");
+				URL url = new URL(address + URLEncoder.encode(query, charset));
+				System.out.println(url.toString());
+				Reader reader = new InputStreamReader(url.openStream(), charset);
+				GoogleResults results = new Gson().fromJson(reader, GoogleResults.class);
+		 
+				int total = results.getResponseData().getResults().size();
+				System.out.println("total: " + total);
+		 
+				// Show title and URL of each results
+				for(int i = 0; i <= total-1; i++){
+					System.out.println("Title: " + results.getResponseData().getResults().get(i).getTitle());
+					System.out.println("URL: " + results.getResponseData().getResults().get(i).getUrl() + "\n");
+				}
 			}
 			
 		}catch(IOException e){
