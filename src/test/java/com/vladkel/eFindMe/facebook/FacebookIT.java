1949141b13eab12d1b2eaaa2ae274056e83479cb
Eliott <eliott@mini-eliott.local>
2014-12-17 14:19:57 +0100

    facebook api tests

--- src/test/java/com/vladkel/eFindMe/facebook/FacebookIT.java
+++ src/test/java/com/vladkel/eFindMe/facebook/FacebookIT.java
@@ -6,22 +6,32 @@ import java.util.List;
 import facebook4j.Facebook;
 import facebook4j.FacebookException;
 import facebook4j.FacebookFactory;
+import facebook4j.Post;
 import facebook4j.ResponseList;
 import facebook4j.User;
 
 public class FacebookIT {
-
+	
+	public static Facebook facebook = new FacebookFactory().getInstance();
+	
+	
 	public static void main(String[] args){
 		
-		Facebook facebook = new FacebookFactory().getInstance();
+		//searchUsers();
 		
+		//getNews();
+		
+		getUser();
+		
+	}
+
+	public static void searchUsers(){
 		String search = "Touzeau";
 		String searchFor = "Olivier Touzeau";
 		
 		List<User> users = new ArrayList<User>();
 		
 		try {
-			
 			ResponseList<User> results = facebook.searchUsers(search);
 			for(User u : results){
 				User user = facebook.getUser(u.getId());
@@ -31,17 +41,67 @@ public class FacebookIT {
 					users.add(user);
 				}
 			}
-			
-			for(User u : users){
-				System.out.println(u.getId() + " ==> " + u);
-			}
-			
-			
-			/*facebook.setOAuthAccessToken(facebook.getOAuthAppAccessToken());
-			facebook.postStatusMessage("Hello World from Facebook4J.");*/
 		} catch (FacebookException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		for(User u : users){
+			System.out.println(u.getId() + " ==> " + u);
+		}
 	}
+	
+	public static void getNews(){
+		
+		try {
+			ResponseList<Post> feed = facebook.getHome();
+			
+			for(Post post : feed){
+				System.out.println("From : " + post.getFrom().getName() + "(" + post.getFrom().getId() + ")");
+				System.out.println(post.getName());
+				System.out.println(post.getDescription());
+				System.out.println();
+			}
+				
+				
+				
+				
+		} catch (FacebookException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	public static void getUser(){
+		
+		// Caro ==> 10205571992638526
+		try {
+			
+			String caro = "10205571992638526";
+			
+			User user = facebook.getUser(caro);
+			
+			System.out.println(user);
+			
+			ResponseList<Post> posts = facebook.getPosts();
+			
+			for(Post post : posts){
+				System.out.println("From : " + post.getFrom().getName() + "(" + post.getFrom().getId() + ")");
+				System.out.println(post.getName());
+				System.out.println(post.getDescription());
+				System.out.println();
+			}
+							
+				
+				
+		} catch (FacebookException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	
+	
 }
